5d93a6516e8fd6a9764d0685c0a9c81d8930f895
Fedor Zernov <fedor11487@mail.ru>
2018-04-15 17:19:02 +0300

    files cleared by not needed headers

--- httpServ0/main.cpp
+++ httpServ0/main.cpp
@@ -1,31 +1,7 @@
-//#ifndef WIN32_LEAN_AND_MEAN
-//#define WIN32_LEAN_AND_MEAN
-//#endif
-#include "Http_Server.h"
-#include <iostream>
-#include <sstream>
-#include <string>
 //для работы с файлами картинок
 #include <stdio.h>
 
-// Для корректной работы freeaddrinfo в MinGW
-// Подробнее: http://stackoverflow.com/a/20306451
-#define _WIN32_WINNT 0x501
-
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#include "base64.h"
-//#include <opencv2\core\core.hpp>
-
-// Необходимо, чтобы линковка происходила с DLL-библиотекой
-// Для работы с сокетам
-#pragma comment(lib, "Ws2_32.lib")
-
-using std::cerr;
-
-#include "FIleRead.h"
-
-
+#include "Http_Server.h"
 
 int main()
 {
